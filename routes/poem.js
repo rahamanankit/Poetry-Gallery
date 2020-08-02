58805878da0d349fc53bf21e943116ec12dbf76f
Ankit Rahaman <32190102+rahamanankit@users.noreply.github.com>
2020-08-02 14:18:42 +0530

    Update poem.js

--- routes/poem.js
+++ routes/poem.js
@@ -1,6 +1,6 @@
 const express 		= require("express");
 const router	 	= express.Router();
-const Poem 			= require("../models/poem");
+const Poem 		= require("../models/poem");
 const middleware 	= require("../middleware");
 
 //INDEX ROUTE - GET - /poems
